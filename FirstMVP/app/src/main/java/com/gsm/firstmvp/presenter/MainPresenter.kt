0586262cf9fb5ddd2e295cec922645cd181ae95d
Im-Tae <dlaxo2420@naver.com>
2020-01-21 18:30:52 +0900

    rm

--- FirstMVP/app/src/main/java/com/gsm/firstmvp/presenter/MainPresenter.kt
+++
@@ -1,9 +0,0 @@
-package com.gsm.firstmvp.presenter
-
-import com.gsm.firstmvp.contact.MainContact
-
-class MainPresenter(override val view: MainContact.View): MainContact.Presenter {
-
-    override fun clickButton() = view.showToast()
-
-}
